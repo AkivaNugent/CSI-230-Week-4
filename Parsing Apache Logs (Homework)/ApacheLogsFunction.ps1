eca31237dc1c94cce59b2babfea15c3f41828bd9
AkivaNugent <AkivaNugent@gmail.com>
2024-09-22 18:02:13 -0400

    Update ApacheLogsFunction.ps1

--- Parsing Apache Logs (Homework)/ApacheLogsFunction.ps1
+++ Parsing Apache Logs (Homework)/ApacheLogsFunction.ps1
@@ -1,12 +1,27 @@
-﻿function ApacheLogs() {
-    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+﻿function ApacheLogs1 {
+    $logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
     $tableRecords = @()
 
-    for($i=0; $i -lt $logsNotFormatted.Count; $i++){
-        $words = $logsNotFormatted[$i].-split(" ");
-
-        $tableRecords += [pscustomobject]@{ "IP"  
-
+    for($i=0; $i -lt $logsNotformatted.Count; $i++){
+        
+        $words = $logsNotformatted[$i] -split " "
+        
+        $tableRecords += [pscustomobject]@{
+            "IP"        = $words[0]
+            "Time"      = $words[3].Trim('[') + " " + $words[4].Trim(']')
+            "Method"    = $words[5].Trim('"')
+            "Page"      = $words[6]
+            "Protocol"  = $words[7].Trim('"')
+            "Response"  = $words[8]
+            "Referrer"  = $words[10].Trim('"')
+            "Client"    = $words[11..($words.Count-1)] -join " "
+        }
     }
 
+    return $tableRecords | Where-Object { $_.IP -like "10.*" }
 }
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
+
+
