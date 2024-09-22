8097de65960abd777b6f837f32d798fbec12ab43
AkivaNugent <143728005+AkivaNugent@users.noreply.github.com>
2024-09-22 18:00:36 -0400

    Working on HW

---
+++ Parsing Apache Logs (Homework)/ApacheLogsFunction.ps1
@@ -0,0 +1,12 @@
+﻿function ApacheLogs() {
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+
+    for($i=0; $i -lt $logsNotFormatted.Count; $i++){
+        $words = $logsNotFormatted[$i].-split(" ");
+
+        $tableRecords += [pscustomobject]@{ "IP"  
+
+    }
+
+}
