b442186f5b22d87e6d4d79da8d14c537457f0a67
AkivaNugent <143728005+AkivaNugent@users.noreply.github.com>
2024-09-22 18:12:23 -0400

    Finished Homework

--- Parsing Apache Logs (Homework)/ApacheLogsFunction.ps1
+++ Parsing Apache Logs (Homework)/ApacheLogsFunction.ps1
@@ -7,14 +7,14 @@
         $words = $logsNotformatted[$i] -split " "
         
         $tableRecords += [pscustomobject]@{
-            "IP"        = $words[0]
-            "Time"      = $words[3].Trim('[') + " " + $words[4].Trim(']')
-            "Method"    = $words[5].Trim('"')
-            "Page"      = $words[6]
-            "Protocol"  = $words[7].Trim('"')
-            "Response"  = $words[8]
-            "Referrer"  = $words[10].Trim('"')
-            "Client"    = $words[11..($words.Count-1)] -join " "
+            "IP"        = $words[0];
+            "Time"      = $words[3].Trim('['); 
+            "Method"    = $words[5].Trim('"');
+            "Page"      = $words[6];
+            "Protocol"  = $words[7];
+            "Response"  = $words[8];
+            "Referrer"  = $words[10];
+            "Client"    = $words[11..($words.Count-1)]
         }
     }
 
