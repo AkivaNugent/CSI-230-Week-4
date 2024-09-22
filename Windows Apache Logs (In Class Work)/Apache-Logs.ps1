94ffb383759da5dd2fc029272b3b50b58535e5d2
AkivaNugent <143728005+AkivaNugent@users.noreply.github.com>
2024-09-22 17:46:53 -0400

    Did the inclass work

---
+++ Windows Apache Logs (In Class Work)/Apache-Logs.ps1
@@ -0,0 +1,16 @@
+﻿function ApacheLogs($page, $code, $browser){
+    $logEntries = Get-Content C:\xampp\apache\logs\access.log | Select-String  -Pattern "$($page) .* $($code) .* $($browser)"
+    
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+    $ipsUnorganized = $regex.Matches($logEntries)
+
+    $ips = @()
+    for ($i=0; $i -lt $ipsUnorganized.Count; $i++){
+        $ips += [pscustomobject ]@{ "IP" = $ipsUnorganized[$i].value;}
+    }
+
+    $ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*"}
+    $counts = $ipsoftens | Group-Object IP | Select-Object Count, Name
+    return $counts
+}
---
+++ Windows Apache Logs (In Class Work)/main.ps1
@@ -0,0 +1,3 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+ApacheLogs "jakewashere" "404" "Chrome"
